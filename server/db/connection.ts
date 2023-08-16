a65efe03e24bb8b35206cc2ef9d562e8170fd14e
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-16 15:09:37 +1200

    remove knex.default after changing moduleResolution to bundler for jwt-auth-query

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -5,6 +5,6 @@ type Environment = 'production' | 'test' | 'development'
 
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
-const connection = knex.default(config)
+const connection = knex(config)
 
 export default connection
