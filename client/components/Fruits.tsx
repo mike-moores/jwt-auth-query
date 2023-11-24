b5d9254e65a59316497497ccf25156ea2ed1e25e
mike moores <michaelmoores@hotmail.com>
2023-11-25 12:26:36 +1300

    passing access tokens

--- client/components/Fruits.tsx
+++ client/components/Fruits.tsx
@@ -5,6 +5,7 @@ import SelectedFruitForm from './SelectedFruit.tsx'
 import AddFruitForm from './AddFruit.tsx'
 import { ErrorMessage } from './Styled.tsx'
 import { useFruits } from '../hooks.ts'
+import { useAuth0 } from '@auth0/auth0-react'
 
 type FormState =
   | {
@@ -17,6 +18,7 @@ type FormState =
     }
 
 function Fruits() {
+  const { getAccessTokenSilently } = useAuth0()
   const [error, setError] = useState('')
   const [form, setForm] = useState<FormState>({
     selectedFruit: null,
@@ -42,25 +44,25 @@ function Fruits() {
     onError: handleError,
   }
 
-  const handleAdd = (fruit: FruitData) => {
+  const handleAdd = async (fruit: FruitData) => {
     // TODO: use getAccessTokenSilently to get an access token
-
+    const token = await getAccessTokenSilently()
     // TODO: pass access token to mutate function
-    fruits.add.mutate({ fruit, token: 'token' }, mutationOptions)
+    fruits.add.mutate({ fruit, token: token }, mutationOptions)
   }
 
-  const handleUpdate = (fruit: Fruit) => {
+  const handleUpdate = async (fruit: Fruit) => {
     // TODO: use getAccessTokenSilently to get an access token
-
+    const token = await getAccessTokenSilently()
     // TODO: pass access token to mutate function
-    fruits.update.mutate({ fruit, token: 'token' }, mutationOptions)
+    fruits.update.mutate({ fruit, token: token }, mutationOptions)
   }
 
-  const handleDeleteFruit = (id: number) => {
+  const handleDeleteFruit = async (id: number) => {
     // TODO: use getAccessTokenSilently to get an access token
-
+    const token = await getAccessTokenSilently()
     // TODO: pass access token to mutate function
-    fruits.delete.mutate({ id, token: 'token' }, mutationOptions)
+    fruits.delete.mutate({ id, token: token }, mutationOptions)
   }
 
   const hideError = () => {
--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,19 +1,18 @@
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { NavGroup, NavButton } from './Styled.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
+  const { user, logout, loginWithRedirect } = useAuth0()
   // TODO: replace placeholder user object with the one from auth0
-  const user = {
-    nickname: 'john.doe',
-  }
 
   const handleSignOut = () => {
-    console.log('sign out')
+    logout()
   }
 
   const handleSignIn = () => {
-    console.log('sign in')
+    loginWithRedirect()
   }
 
   return (
